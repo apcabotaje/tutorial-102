6da072b0ca0a42fecb556027992def30e259c832
Ayman Cabotaje <ayman@drtalks.com>
2023-11-22 19:50:51 +0800

    initial commit

---
+++ assets/scripts/script.js
@@ -0,0 +1,7 @@
+const body = document.body 
+
+const div = document.querySelector('div')
+const spanHello = document.querySelector('#hello')
+const spanBye = document.querySelector('#bye')
+
+spanHello.style.backgroundColor = "blue"
